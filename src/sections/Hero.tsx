0cc49b71adb8ae8a9dfa127de1b1bc3ae20e2329
Török Ádám <adam.torok@xtradevs.com>
2026-01-28 01:29:12 +0100

    chore: init tailwind

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,9 @@
 export const Hero = () => {
     return (
         <div>
-            <h1>hero</h1>
+            <h1 className="text-3xl font-bold underline text-blue-600">
+                Teszt teszt
+            </h1>
         </div>
     );
 }
